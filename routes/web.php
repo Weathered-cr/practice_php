a064b67dee755212a02e1624c513cef9f0279a9d
Weathered-cr <mrfocs2000@gmail.com>
2025-05-16 11:40:16 +0700

    task_2_login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Src\Route;
 Route::add('go', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
 Route::add('go/signup', [Controller\Site::class, 'signup']);
+Route::add('go/login', [Controller\Site::class, 'login']);
+Route::add('go/logout', [Controller\Site::class, 'logout']);
