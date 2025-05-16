b41a670a75f0a567b2a0a866fa9cd3f3cd3db02c
Weathered-cr <mrfocs2000@gmail.com>
2025-05-16 09:40:11 +0700

    task_2_bd

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'gshzumtd_m6',
+    'username' => 'gshzumtd',
+    'password' => 'cKHTfK',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
--- core/Src/Settings.php
+++ core/Src/Settings.php
@@ -27,4 +27,8 @@ class Settings
     {
         return '/' . $this->path['views'] ?? '';
     }
+    public function getDbSetting(): array
+    {
+        return $this->db ?? [];
+    }
 }
